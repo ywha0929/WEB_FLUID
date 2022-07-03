0915040fbc55b305248b48ea251090c3c9f975de
ywha0929 <ywha0929@gmail.com>
2022-07-04 01:18:22 +0900

    unmodified project for text app

--- TESTAPP_text/app/src/main/java/com/example/testapp/MainActivity.java
+++ TESTAPP_text/app/src/main/java/com/example/testapp/MainActivity.java
@@ -28,9 +28,6 @@ public class MainActivity extends AppCompatActivity {
     private Button btn1;
     private Button btn2;
 
-    Class<?> clazz = null;
-    Method mtd;
-
     //
 
     @Override
@@ -42,37 +39,11 @@ public class MainActivity extends AppCompatActivity {
         edit2 = (EditText) findViewById(R.id.edit2);
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
-        Activity a = this;
-        DexClassLoader dex = new DexClassLoader("/data/local/tmp/fluidlib.apk", "/data/local/tmp/", null, getClass().getClassLoader());
-        try {
-            Log.d("TAG", "TRY started");
-            clazz = dex.loadClass("com.hmsl.fluidlib.FLUIDMain");
-            Log.d("TAG", "clazz : " + clazz.getClass().toString());
-            Object obj = clazz.newInstance();
-            Log.d("TAG", "obj = " + obj.getClass().toString());
-            obj = clazz.getDeclaredField("mServiceConnection").get(obj);
-            Intent intent = new Intent("com.hmsl.fluidmanager.MY_Service");
-            intent.setPackage("com.hmsl.fluidmanager");
-            //this();
-            intent.setClassName("com.hmsl.fluidmanager", "com.hmsl.fluidmanager.FLUIDManagerService");
-            Boolean bool = bindService(intent, (ServiceConnection) obj, Context.BIND_AUTO_CREATE);
-            Log.d("TAG", "is bind : " + bool);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
 
         edit1.setOnLongClickListener(new View.OnLongClickListener() {//distribute trigger
             @Override
             public boolean onLongClick(View v) {
-                try {
-                    Log.d("TAG", "edit1 invoked");
-                    mtd = clazz.getDeclaredMethod("runtest", String.class,View.class);
-                    mtd.invoke(null, "EditText", v);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return false;
-                }
+
 
                 return true;
             }
@@ -81,14 +52,6 @@ public class MainActivity extends AppCompatActivity {
         edit2.setOnLongClickListener(new View.OnLongClickListener() {//distribute trigger
             @Override
             public boolean onLongClick(View v) {
-                try {
-                    Log.d("TAG", "edit2 invoked");
-                    mtd = clazz.getDeclaredMethod("runtest", String.class,View.class);
-                    mtd.invoke(null, "EditText", v);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return false;
-                }
 
                 return true;
             }
@@ -97,65 +60,33 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnLongClickListener(new View.OnLongClickListener() {//distribute trigger
             @Override
             public boolean onLongClick(View v) {
-                try {
-                    Log.d("TAG", "btn1 invoked");
-                    mtd = clazz.getDeclaredMethod("runtest", String.class,View.class);
-                    mtd.invoke(null, "Button", v);
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return false;
-                }
 
                 return true;
             }
         });
-        btn1.setOnClickListener(new View.OnClickListener()
-        {
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("TAG", "btn1 short invoked");
                 edit1.setTextColor(Color.BLUE);
-                try {
-                    mtd = clazz.getDeclaredMethod("runUpdate",String.class,View.class,Object.class);
-                    mtd.invoke(null,"setTextColor",edit1,Color.BLUE);
-                } catch(Exception e)
-                {
-                    e.printStackTrace();
-                    return;
-                }
+
             }
         });
         btn2.setOnLongClickListener(new View.OnLongClickListener() { //distribute trigger
             @Override
             public boolean onLongClick(View v) {
-                try {
-                    Log.d("TAG", "btn2 invoked");
-                    mtd = clazz.getDeclaredMethod("runtest", String.class,View.class);
-                    mtd.invoke(null, "Button", v);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return false;
-                }
+
                 return true;
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener()
-        {
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("TAG", "btn2 short invoked");
                 edit2.setTextSize(90);
-                try {
-                    mtd = clazz.getDeclaredMethod("runUpdate",String.class,View.class, Object.class);
-                    float a = 200.0f;
-                    mtd.invoke(null,"setTextSize",edit2,a);
-                } catch(Exception e)
-                {
-                    e.printStackTrace();
-                    return;
-                }
+
 
             }
         });
