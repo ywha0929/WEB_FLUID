208f593b6a2a896e48a874a57cb9aacd80c90972
ywha0929 <ywha0929@gmail.com>
2022-07-05 18:36:58 +0900

    rollback

---
+++ FLUID_Interface/push.sh
@@ -0,0 +1,3 @@
+cp /home/ywha/WEB_FLUID/FLUID_Interface/app/release/app-release.apk /home/ywha/WEB_FLUID/FLUID_Interface/app/release/fluidlib.apk
+adb -s 12191FDD40010L push /home/ywha/WEB_FLUID/FLUID_Interface/app/release/fluidlib.apk /data/local/tmp
+
