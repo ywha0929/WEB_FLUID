3b7496a9639d659aa71f4e7d580824fde823f56d
ywha0929 <ywha0929@gmail.com>
2022-08-08 23:14:10 +0900

    Injected ImageManager

--- ImageManager/app/src/main/java/com/example/imagemanager/MainActivity.java
+++ ImageManager/app/src/main/java/com/example/imagemanager/MainActivity.java
@@ -20,25 +20,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        back_btn = findViewById(R.id.button1);
         next_btn = findViewById(R.id.button2);
         imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.stepone);
+
+        back_btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                return true;
+            }
+        });
+
+        next_btn.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View view) {
+                return true;
+            }
+        });
+
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                return true;
+            }
+        });
 
         next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                state++;
                 if (state == 0) {
                     imageView.setImageResource(R.drawable.stepone);
-                    state++;
+//                    state++;
                 }
                 else if (state == 1) {
                     imageView.setImageResource(R.drawable.steptwo);
-                    state++;
+//                    state++;
                 }
                 else if (state ==2) {
                     imageView.setImageResource(R.drawable.stepthree);
-                    state++;
+//                    state++;
                 }
                 else {
+                    state=2;
                     Toast.makeText(getApplicationContext(), "This is the last image.", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -47,25 +72,32 @@ public class MainActivity extends AppCompatActivity {
 
 
         back_btn = findViewById(R.id.button1);
-        imageView = findViewById(R.id.imageView);
+//        imageView = findViewById(R.id.imageView);
 
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                state--;
                 if (state == 0) {
-                    Toast.makeText(getApplicationContext(), "This is the first image.", Toast.LENGTH_SHORT).show();
+//                    state=0;
+                    imageView.setImageResource(R.drawable.stepone);
+//                    Toast.makeText(getApplicationContext(), "This is the first image.", Toast.LENGTH_SHORT).show();
                 }
                 else if (state == 1) {
-                    imageView.setImageResource(R.drawable.stepone);
-                    state--;
+                    imageView.setImageResource(R.drawable.steptwo);
+//                    imageView.setImageResource(R.drawable.stepone);
+//                    state--;
                 }
                 else if (state == 2) {
-                    imageView.setImageResource(R.drawable.steptwo);
-                    state--;
-                }
-                else {
                     imageView.setImageResource(R.drawable.stepthree);
-                    state--;
+//                    imageView.setImageResource(R.drawable.steptwo);
+//                    state--;
+                }
+                else {//if state < 0
+                    state=0;
+                    Toast.makeText(getApplicationContext(), "This is the first image.", Toast.LENGTH_SHORT).show();
+//                    imageView.setImageResource(R.drawable.stepthree);
+//                    state--;
                 }
             }
         });
