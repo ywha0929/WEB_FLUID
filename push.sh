73285e67a96a091f25d025bc987240a2edc9f792
ywha0929 <ywha0929@gmail.com>
2022-10-02 21:49:58 +0900

    1002

--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
 git add .
 git add -f FLUID_Injector/apks/*.apk FLUID_Injector/ext_apks/*.apk FLUID_Injector/output/*.apk
+git add -f react-proxy/node_modules
+git add -f react-proxy/node_modules/* -r
 git commit -m $1
 git push
