780023ec904ec3d95dcbd1f4027abc2340028a5f
ywha0929 <ywha0929@gmail.com>
2022-10-16 01:18:17 +0900

    for_mid_report

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git add .
-#git add -f FLUID_Injector/apks/*.apk FLUID_Injector/ext_apks/*.apk FLUID_Injector/output/*.apk
+git add -f FLUID_Injector/apks/*.apk FLUID_Injector/ext_apks/*.apk FLUID_Injector/output/*.apk
 git commit -m $1
 git push
